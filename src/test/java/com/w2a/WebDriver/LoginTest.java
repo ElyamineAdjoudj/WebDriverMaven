4002f98d7511785891047e542bb1cd2b3617b313
lamine8819 <lamine8819@gmail.com>
2021-03-06 19:10:49 -0500

    first commit

---
+++ src/test/java/com/w2a/WebDriver/LoginTest.java
@@ -0,0 +1,29 @@
+package com.w2a.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	@Test
+	public void logintest() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+	    driver.get("http://practice.automationtesting.in/my-account/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("pavanoltraining");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("Test@selenium123");
+		driver.findElement(By.xpath("//*[@id=\"customer_login\"]/div[1]/form/p[3]/input[3]")).click();
+		//ADD new comments
+		//new comment
+		
+		driver.close();
+}
+}
